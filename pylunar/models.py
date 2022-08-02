bbea4631ccb6fc7ac8f1693ead14df2704b9f34b
WinterFe <winter@lunardev.group>
2022-08-02 16:26:49 -0400

    make it easier to send files in discord + fixes

--- pylunar/models.py
+++ pylunar/models.py
@@ -1,5 +1,6 @@
+from io import BytesIO
 import aiohttp
-
+import discord
 
 class BaseModel:
     def __init__(self, response: aiohttp.ClientResponse):
@@ -9,6 +10,10 @@ class BaseModel:
 class Image(BaseModel):
     async def bytes(self):
         return await self.response.read()
+    
+    async def file(self):  # sourcery skip: avoid-builtin-shadow
+        b = await self.response.read()
+        return discord.File(BytesIO(b), "image.png")
 
     async def save(self, fp: str):
         with open(fp, "wb") as f:
