ea45818f959c5ee2c6e7a06a52c9120c8062e873
zeffo <phatakaman@gmail.com>
2022-08-02 17:22:02 +0530

    feat: update example

--- examples/example.py
+++ examples/example.py
@@ -7,7 +7,12 @@ token = ""  # Replace with your Lunar API Key.
 async def main():
     async with ClientSession() as session:
         client = Client(session=session, token=token)
+
         image = await client.request(endpoints.generate_achievement, text="Woo! I made a request!")
-        await image.save("image.jpg")
+        await image.save("image.jpg")  
+
+        nsfw_jpg = await client.request(endpoints.nsfw("jpg"))
+        data = await nsfw_jpg.to_dict()
+        print("Image URL is", data["url"])
 
 asyncio.run(main())
