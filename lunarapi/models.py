460e2139baf5e8d80cb6d2c3e6335e9eb6d27693
Soheab <Soheab@users.noreply.github.com>
2022-08-21 20:57:48 +0200

    Add support for older python version
    Specifically, 3.8 and 3.9.
    
    Also improves some typing stuff.

--- lunarapi/models.py
+++ lunarapi/models.py
@@ -1,23 +1,35 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any
+
 from io import BytesIO
 import aiohttp
 
+if TYPE_CHECKING:
+    from typing import Callable, Protocol, Union
+
+    from aiohttp import ClientResponse
+
+    class DiscordT(Protocol):
+        File: Callable[[Union[BytesIO, Any], str], Any]
+else:
+    DiscordT: Any
 class BaseModel:
-    def __init__(self, response: aiohttp.ClientResponse):
-        self.response = response
+    def __init__(self, response: ClientResponse) -> None:
+        self.response: ClientResponse = response
 
 
 class Image(BaseModel):
-    async def bytes(self):
+    async def bytes(self) -> bytes:
         return await self.response.read()
     
-    async def file(self, discord):  # sourcery skip: avoid-builtin-shadow
+    async def file(self, module: DiscordT):  # sourcery skip: avoid-builtin-shadow
         b = await self.response.read()
-        return discord.File(BytesIO(b), "image.png")
+        return module.File(BytesIO(b), "image.png")
 
-    async def save(self, fp: str):
+    async def save(self, fp: str) -> None:
         with open(fp, "wb") as f:
             f.write(await self.bytes())
 
 class JSON(BaseModel):
-    async def to_dict(self):
+    async def to_dict(self) -> dict[Any, Any]:
         return await self.response.json()
